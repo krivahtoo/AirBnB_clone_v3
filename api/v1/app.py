93187d289b14e9b02cb4fdf912febb43666eb273
Noah Too <krivahtoo@gmail.com>
2024-08-05 10:42:19 +0300

    Add Task 5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 0x05. AirBnB clone - RESTful API
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -16,6 +16,13 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """Method to handle app context teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors that returns a JSON-formatted response"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
